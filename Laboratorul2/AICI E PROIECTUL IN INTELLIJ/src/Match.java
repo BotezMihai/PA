b28ebdaf04c407741139cd3fde4abf12c339f1f6
Dragomas <36089404+Dragomas@users.noreply.github.com>
2019-03-03 22:29:57 +0200

    Am adaugat cateva chestii

--- Laboratorul2/AICI E PROIECTUL IN INTELLIJ/src/Match.java
+++ Laboratorul2/AICI E PROIECTUL IN INTELLIJ/src/Match.java
@@ -1,4 +1,49 @@
+import java.util.*;
+import java.util.stream.*;
+java.util.Set;
+        java.util.HashSet
 public class Match {
+
+    private Map matches;
+    private List<Student> students;
+    private ArrayList<Project> projects;
+    public Match(List<Student> stud, ArrayList<Project> proj)
+    {
+        students=stud;
+        projects=proj;
+        matches=new HashMap();
+
+    }
+
+    public Project checkProject(Student student)
+    {
+        ArrayList<Project> projects1=student.getPreferences();
+        int i,j;
+        for(i=0;i<projects1.size();i++)
+        {
+            if(matches.containsValue(projects1.get(i).getName())==false)
+                return projects1.get(i);
+
+        }
+        for(i=0;i<projects1.size();i++)
+        {
+            if(matches.containsValue(projects1.get(i).getName())==true)
+            {
+                Set set = matches.entrySet();
+
+            }
+        }
+    }
+    public void makeMatches(){
+
+        int i;
+        for(i=0;i<students.size();i++)
+        {
+            Project p=checkProject(students.get(i));
+            matches.put(students.get(i),p);
+        }
+
+    }
 
 
 }
