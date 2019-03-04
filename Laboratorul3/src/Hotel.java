05cf5e33e2a954d41cde80dba03b7b74d142f769
mihai <mihai_b9812@yahoo.com>
2019-03-04 14:27:59 +0200

    Am adaugat implementarea de interfete

--- Laboratorul3/src/Hotel.java
+++ Laboratorul3/src/Hotel.java
@@ -2,12 +2,16 @@
  * @author Botez Mihai
  * @author Dragomir Cristian
  */
-public class Hotel extends Node implements Classifiable{
+import java.util.Scanner;
+public class Hotel extends Node implements Classifiable, Payable{
 
-       String rank;
+      private String rank;
+      private int singleRoom;
+      private int doubleRoom;
        public Hotel (String nume)
        {
            super(nume);
+           singleRoom=doubleRoom=0;
        }
   public void setRank(int s)
   {
@@ -18,6 +22,33 @@ public class Hotel extends Node implements Classifiable{
   {
       return rank;
   }
+   public void setEntryFee(int s)
+   {
+          System.out.println("Set this for 1. Single Room or for 2. Double Room");
+       Scanner sc = new Scanner(System.in);
+       int option = sc.nextInt();
+       if(option==1)
+           singleRoom=s;
+       else if(option==2)
+       {
+           doubleRoom=s;
+       }
+       else {
+           System.out.println("Wrong number");
+       }
+   }
+   public int getEntryFee()
+   {
+       System.out.println("Price for single room 1, for double room is 2");
+       Scanner sc = new Scanner(System.in);
+       int option = sc.nextInt();
+       if(option==1)
+           return singleRoom;
+       else if (option==2)
+           return doubleRoom;
+       else
+           return 0;
+   }
 
 
 }
--- Laboratorul3/src/Main.java
+++ Laboratorul3/src/Main.java
@@ -7,6 +7,7 @@ public class Main {
         Hotel v1=new Hotel("California");
        System.out.println(v1.getName());
        Museum v2=new Museum("Iasi");
+       System.out.println(v1.getEntryFee());
        System.out.println(v2.getName());
        v2.setOpeningHour(LocalTime.of(10,14));
        System.out.println(v2.getOpeningHour());
